3b063fcf2edc5cabf2712bb9879c086f698621b7
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-16 02:27:47 +0530

    notification list imported

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import NotificationsList from '../Notifications/NotificationsList';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <main className={classNames(styles.home)}>
       <Navbar />
+      <NotificationsList />
     </main>
   );
 };
