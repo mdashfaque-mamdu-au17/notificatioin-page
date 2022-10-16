ac1a9063e1c708e8f8e9c400c7f49453f9d77672
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-16 23:22:39 +0530

    state management for notification clearing functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,21 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import NotificationsList from '../Notifications/NotificationsList';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const [notificationCount, setNotificationCount] = useState(3);
+  const markAllReadClickHandler = () => {
+    setNotificationCount(0);
+  };
   return (
     <main className={classNames(styles.home)}>
-      <Navbar />
-      <NotificationsList />
+      <Navbar
+        notificationCount={notificationCount}
+        onReadAll={markAllReadClickHandler}
+      />
+      <NotificationsList notificationCount={notificationCount} />
     </main>
   );
 };
