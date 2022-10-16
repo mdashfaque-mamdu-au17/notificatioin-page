85ddc54a4233a7a3a69acb147286f56b303b4566
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-16 23:23:07 +0530

    dynamic notifiaction count added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,17 @@
 import classNames from 'classnames';
 import React from 'react';
 import styles from './Navbar.module.css';
-const Navbar = () => {
+const Navbar = ({ notificationCount, onReadAll }) => {
   return (
     <nav className={classNames(styles.navbar)}>
-      <div
-        className={classNames(styles.navbar__notification__container)}
-      >
+      <div className={classNames(styles.navbar__notification__container)}>
         <h5>Notifications</h5>
         <div className={classNames(styles.navbar__badge)}>
-          <span>3</span>
+          <span>{notificationCount}</span>
         </div>
       </div>
       <div>
-        <button className={classNames(styles.clear__btn)}>
+        <button className={classNames(styles.clear__btn)} onClick={onReadAll}>
           Mark all as read
         </button>
       </div>
